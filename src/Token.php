57fc04ae08bb1a876209aaa264f7911dd8652fc7
wenhainan <whndeweilai@163.com>
2022-01-07 17:03:56 +0800

    修改命名，更合理

--- src/Token.php
+++ src/Token.php
@@ -1,99 +1,157 @@
 <?php
-/**
- * auther by wenhainan
- * email  whndeweilai@163.com
- * QQ     610176732
- * nickname  阿修罗
- */
+
 namespace think\wenhainan;
 
+use think\wenhainan\Driver;
+
 
 /**
- * 如果你的驱动是mysql，你需要创建个表
-     CREATE TABLE `user_token` (
-    `token` varchar(50)  NOT NULL COMMENT 'Token',
-    `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '会员ID',
-    `createtime` int(11) DEFAULT NULL COMMENT '创建时间',
-    `expiretime` int(11) DEFAULT NULL COMMENT '过期时间',
-    PRIMARY KEY (`token`) USING BTREE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='会员Token表';
+ * Token操作类
  */
-
-/**
- * Token基础类
- */
-abstract class Token
+class Token
 {
-    protected $handler = null;
-    protected $options = [];
+    /**
+     * @var array Token的实例
+     */
+    public static $instance = [];
 
     /**
-     * 存储Token
-     * @param   string $token   Token
-     * @param   int    $user_id 会员ID
-     * @param   int    $expire  过期时长,0表示无限,单位秒
+     * @var object 操作句柄
+     */
+    public static $handler;
+
+    /**
+     * 连接Token驱动
+     * @access public
+     * @param  array       $options 配置数组
+     * @param  bool|string $name    Token连接标识 true 强制重新连接
+     * @return Driver
+     */
+    public static function connect(array $options = [], $name = false)
+    {
+        $type = !empty($options['type']) ? $options['type'] : 'File';
+
+        if (false === $name) {
+            $name = md5(serialize($options));
+        }
+
+        if (true === $name || !isset(self::$instance[$name])) {
+            $class = false === strpos($type, '\\') ?
+                '\\think\\wenhainan\\driver\\' . ucwords($type) :
+                $type;
+
+            // 记录初始化信息
+            //App::$debug && Log::record('[ TOKEN ] INIT ' . $type, 'info');
+
+            if (true === $name) {
+                return new $class($options);
+            }
+
+            self::$instance[$name] = new $class($options);
+        }
+
+        return self::$instance[$name];
+    }
+
+    /**
+     * 自动初始化Token
+     * @access public
+     * @param  array $options 配置数组
+     * @return Driver
+     */
+    public static function init(array $options = [])
+    {
+    	
+        if (is_null(self::$handler)) {
+            if (empty($options) && 'complex' == config('token.type')) {
+                $default = config('token.default');
+                // 获取默认Token配置，并连接
+                $options = config('token.' . $default['type']) ?: $default;
+            } elseif (empty($options)) {
+                $options = config('token');
+            }
+            self::$handler = self::connect($options);
+        }
+
+        return self::$handler;
+    }
+
+    /**
+     * 判断Token是否可用(check别名)
+     * @access public
+     * @param  string $token Token标识
      * @return bool
      */
-    abstract function set($token, $user_id, $expire = 0);
-
-    /**
-     * 获取Token内的信息
-     * @param   string $token
-     * @return  array
-     */
-    abstract function get($token);
+    public static function has($token, $user_id)
+    {
+        return self::check($token, $user_id);
+    }
 
     /**
      * 判断Token是否可用
-     * @param   string $token   Token
-     * @param   int    $user_id 会员ID
-     * @return  boolean
+     * @param string $token Token标识
+     * @return bool
      */
-    abstract function check($token, $user_id);
+    public static function check($token, $user_id)
+    {
+        return self::init()->check($token, $user_id);
+    }
+
+    /**
+     * 读取Token
+     * @access public
+     * @param  string $token   Token标识
+     * @param  mixed  $default 默认值
+     * @return mixed
+     */
+    public static function get($token, $default = false)
+    {
+        return self::init()->get($token, $default);
+    }
+
+    /**
+     * 写入Token
+     * @access public
+     * @param  string   $token   Token标识
+     * @param  mixed    $user_id 存储数据
+     * @param  int|null $expire  有效时间 0为永久
+     * @return boolean
+     */
+    public static function set($token, $user_id, $expire = null)
+    {
+        return self::init()->set($token, $user_id, $expire);
+    }
+
+    /**
+     * 删除Token(delete别名)
+     * @access public
+     * @param  string $token Token标识
+     * @return boolean
+     */
+    public static function rm($token)
+    {
+        return self::delete($token);
+    }
 
     /**
      * 删除Token
-     * @param   string $token
-     * @return  boolean
+     * @param string $token 标签名
+     * @return bool
      */
-    abstract function delete($token);
+    public static function delete($token)
+    {
+        return self::init()->delete($token);
+    }
 
     /**
-     * 删除指定用户的所有Token
-     * @param   int $user_id
-     * @return  boolean
-     */
-    abstract function clear($user_id);
-
-    /**
-     * 返回句柄对象，可执行其它高级方法
-     *
+     * 清除Token
      * @access public
-     * @return object
+     * @param  string $token Token标记
+     * @return boolean
      */
-    public function handler()
+    public static function clear($user_id = null)
     {
-        return $this->handler;
+        return self::init()->clear($user_id);
     }
 
-    /**
-     * 获取加密后的Token
-     * @param string $token Token标识
-     * @return string
-     */
-    protected function getEncryptedToken($token)
-    {
-        $config = config('token');
-        return hash_hmac($config['hashalgo'], $token, $config['key']);
-    }
-
-    /**
-     * 获取过期剩余时长
-     * @param $expiretime
-     * @return float|int|mixed
-     */
-    protected function getExpiredIn($expiretime)
-    {
-        return $expiretime ? max(0, $expiretime - time()) : 365 * 86400;
-    }
 }
